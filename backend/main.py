68c46e166b2e2de86d9697f6dc13bb6cbfec8d3d
Vladislav Supalov <v.supalov@pivii.co>
2017-04-27 10:27:12 +0200

    Dummy backend data.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,36 @@
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/api/v1/search")
+def search():
+    response = {
+        "tags": [
+           {
+                "name": "something",
+                "count": 5 
+           }
+        ]
+    }
+
+    return json.dumps(response)
+
+@app.route("/api/v1/related")
+def related():
+    response = {
+        "tags_large": [
+           {"name": "something", "count": 5}
+        ],
+        "tags_medium": [
+           {"name": "something", "count": 5}
+        ],
+        "tags_small": [
+           {"name": "something", "count": 5}
+        ]
+    }
+
+    return json.dumps(response)
 
 if __name__ == "__main__":
     app.run()
