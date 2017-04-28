e1bdbe06576d5128b04cfe2b34460d9cac5043df
Vladislav Supalov <v.supalov@gmail.com>
2017-04-28 12:36:29 +0200

    Less boring dummy data.

--- backend/main.py
+++ backend/main.py
@@ -3,31 +3,28 @@ import json
 from flask import Flask
 app = Flask(__name__)
 
+dummy = ["a","b","c"]
+
 @app.route("/api/v1/search")
 def search():
     response = {
-        "tags": [
-           {
-                "name": "something",
-                "count": 5 
-           }
-        ]
+        "tags": dummy_item_list("try ", dummy)
     }
 
     return json.dumps(response)
 
+def dummy_item_list(prefix, suffices):
+    return map(lambda x: {
+                "name": "{}something{}".format(prefix,x),
+                "count": 5 
+           }, suffices)
+
 @app.route("/api/v1/related")
 def related():
     response = {
-        "tags_large": [
-           {"name": "something", "count": 5}
-        ],
-        "tags_medium": [
-           {"name": "something", "count": 5}
-        ],
-        "tags_small": [
-           {"name": "something", "count": 5}
-        ]
+        "tags_large": dummy_item_list("l ", dummy),
+        "tags_medium": dummy_item_list("m ", dummy),
+        "tags_small": dummy_item_list("s ", dummy)
     }
 
     return json.dumps(response)
