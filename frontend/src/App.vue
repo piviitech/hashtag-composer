59016a043c8499cedc1ba5a762fa3bdd3637b35f
Vladislav Supalov <v.supalov@gmail.com>
2017-04-28 07:12:13 +0200

    Let's go for the sweet sweet prototyping :)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -11,13 +10,12 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass" scoped>
+$font-stack: Helvetica, sans-serif;
+$primary-color: #333;
+
+#app
+  font-family: $font-stack
+  margin-top: 20px
+  margin-left: 20px
 </style>
